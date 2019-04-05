9cb21caeba6fc16b5c66f4e0675251064f63abd0
hadoyun178 <48614550+hadoyun178@users.noreply.github.com>
2019-04-05 13:34:15 +0900

    using console API

--- test.cpp
+++ test.cpp
@@ -13,24 +13,24 @@ void MySetConsoleCursorPosition(SHORT x, SHORT y)
 
 void PrintAt(SHORT x, SHORT y, char letter)
 {
-	MySetConsoleCursorPosition( x, y );
+	MySetConsoleCursorPosition(x, y);
 	cout << letter;
 }
 
 void PrintAt(SHORT x, SHORT y, const char* str)
 {
-	MySetConsoleCursorPosition( x, y );
+	MySetConsoleCursorPosition(x, y);
 	cout << str;
 }
 
-int main()
+int main__()
 {
 	g_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	
+
 	PrintAt(6, 4, '$');
 	PrintAt(4, 2, '@');
 	PrintAt(1, 7, '$');
-	PrintAt(10, 3, "test" );
+	PrintAt(10, 3, "test");
 
 	MySetConsoleCursorPosition(0, 10);
 
